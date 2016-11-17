c6ea488bdb8eefcd3e886b58499a4dc0a6e7ff4e
Jose Amezcua <amezcua.jose29@gmail.com>
2016-11-17 08:52:55 -0800

    part two check

---
+++ java.js
@@ -0,0 +1,31 @@
+var BatX = 50;
+var BatY = 50;
+document.addEventListener("keydown", function(e) {
+ if(e.keyCode == 37){
+   BatX -=15;
+document.getElementById("bat").setAttribute("x", BatX)
+
+ }
+ else if(e.keyCode == 39){
+       BatX +=15;
+document.getElementById("bat").setAttribute("x", BatX)
+ }
+
+ if(e.keyCode == 37){
+   BatX -=15;
+document.getElementById("batFly").setAttribute("x", BatX)
+
+ }
+ else if(e.keyCode == 39){
+       BatX +=15;
+document.getElementById("batFly").setAttribute("x", BatX)
+ }
+ else if(e.keyCode == 38){
+       BatY -=15;
+document.getElementById("batFly").setAttribute("y", BatY)
+ }
+ else if(e.keyCode == 40){
+       BatY +=15;
+document.getElementById("batFly").setAttribute("y", BatY)
+ }
+})
