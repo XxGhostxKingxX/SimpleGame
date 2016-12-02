e81cb2dd6b7144cba2d6080927d226e86388a929
Jose Amezcua <amezcua.jose29@gmail.com>
2016-12-02 08:44:36 -0800

    Fixing overlap and score

--- java.js
+++ java.js
@@ -1,35 +1,21 @@
 var canvas1 = document.getElementById("overlap-canvas-one")
 var BatX = 525;
 var BatY = 300;
+var food1 =
+var food2 =
+var foodEaten = 0;
+
 document.addEventListener("keydown", function(e) {
  if(e.keyCode == 37){
    BatX -=15;
 document.getElementById("bat").setAttribute("x", BatX)
 
  }
- else if(e.keyCode == 39){
+ else (e.keyCode == 39){
        BatX +=15;
 document.getElementById("bat").setAttribute("x", BatX)
  }
-
- if(e.keyCode == 37){
-   BatX -=15;
-document.getElementById("batFly").setAttribute("x", BatX)
-
- }
- else if(e.keyCode == 39){
-       BatX +=15;
-document.getElementById("batFly").setAttribute("x", BatX)
- }
- else if(e.keyCode == 38){
-       BatY -=15;
-document.getElementById("batFly").setAttribute("y", BatY)
- }
- else if(e.keyCode == 40){
-       BatY +=15;
-document.getElementById("batFly").setAttribute("y", BatY)
- }
-
+}
  var Bat1 = document.getElementById("Bat1")
  var food1 = document.getElementById("food1")
 
@@ -39,16 +25,24 @@ document.getElementById("batFly").setAttribute("y", BatY)
  foodX = Number(food1.getAttribute("x"))
  foodY = Number(food1.getAttribute("y"))
 
- if (BatX > foodX && batX < foodX + width) {
-   bat1.setAttribute("opacity", "0")
-   food1.setAttribute("opacity", "0")
- }
- else {
-   food1.setAttribute("opacity", "0")
- }
+}
+if(BatX > foodX && BatX < foodX + width && BatY > foodY && BatY < foodY + height) {
+  foodEaten = foodEaten + 1;
+  document.getElementById("score").textContent = foodEaten;
+}
+
+foodX = Number(food1.getAttribute("x"))
+foodY = Number(food1.getAttribute("y"))
+
+if (BatX > foodX && BatX < foodX + width && BatY > foodY && BatY < foodY + height) {
+  var xValue = randomNumber();
+  var yValue = randomNumber();
+  foodEaten = foodEaten + 1;
+  document.getElementById("score").textContent = foodEaten;
+}
+
+else {
+  foodEaten.setAttribute("opacity", 1)
+}
 
 })
-
-if(BatX > foodX && BatX < foodX + width && BatY > foodY && BatY < foodY + height) {
-  //code to "eat" food Part 5
-}
